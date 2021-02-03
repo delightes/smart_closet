0c56d9b953207529df959d8cb27fd245b4692590
eunsoo <skfmf7878@naver.com>
2021-02-03 21:54:54 +0900

    2021-02-02 수정 1. 하의 화면 제작 2. 배경 화면 제작

--- app/src/main/java/com/example/smartcloset/StartActivity.java
+++ app/src/main/java/com/example/smartcloset/StartActivity.java
@@ -1,8 +1,9 @@
-//2. 상의 화면
+//2. 선택화면
 package com.example.smartcloset;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,82 +15,122 @@ import android.widget.LinearLayout;
 
 public class StartActivity extends AppCompatActivity implements OnClickListener {
 
-    ImageButton shirts_bt, tshirt_bt, sweater_bt, vest_bt;
-    ImageButton skin_btn_1, skin_btn_2, skin_btn_3, skin_btn_4, skin_btn_5; // 피부색 버튼
-    ImageButton hair_btn_1, hair_btn_2, hair_btn_3, hair_btn_4, hair_btn_5; // 머리 버튼
-    ImageButton eye_btn_1, eye_btn_2, eye_btn_3, eye_btn_4; // 눈 버튼
-    ImageButton mouth_btn_1, mouth_btn_2, mouth_btn_3, mouth_btn_4; // 입 버튼
-    ImageView iv, skin_p, hair_view, eye_view, mouth_view;
-    LinearLayout linearLayout1, linearLayout2, linearLayout3, linearLayout4, linearLayout5; // 레이아웃
-    Button skin_button, hair_button, eye_button, mouth_button, top_button, bottom_button; // 상단버튼
+    /* 1) 변수 선언 */
+    LinearLayout linearLayout1, linearLayout2, linearLayout3, linearLayout4, linearLayout5, linearLayout6, linearLayout7; // 레이아웃
+    ImageView skin_p, hair_view, eye_view, mouth_view, top_view, bottom_view, bg_view; //이미지 뷰
+    Button skin_button, hair_button, eye_button, mouth_button, top_button, bottom_button, bg_button; //바꿀 부분 선택 버튼
+    ImageButton skin_btn_1, skin_btn_2, skin_btn_3, skin_btn_4, skin_btn_5; // 피부색1~5 버튼
+    ImageButton hair_btn_1, hair_btn_2, hair_btn_3, hair_btn_4, hair_btn_5; // 머리1~5 버튼
+    ImageButton eye_btn_1, eye_btn_2, eye_btn_3, eye_btn_4; // 눈1~4 버튼
+    ImageButton mouth_btn_1, mouth_btn_2, mouth_btn_3, mouth_btn_4; // 입1~4 버튼
+    ImageButton shirts_bt, tshirt_bt, sweater_bt, vest_bt; //상의1~4 버튼
+    ImageButton bottom_btn_1, bottom_btn_2, bottom_btn_3, bottom_btn_4, bottom_btn_5; //하의1~5 버튼
+    ImageButton bg_btn_1, bg_btn_2, bg_btn_3, bg_btn_4, bg_btn_5, bg_btn_6, bg_btn_7, bg_btn_8; //배경색1~8 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        //이미지뷰를 버튼에 넣어 속성 사용하기 위한 구문
-        shirts_bt = (ImageButton)findViewById(R.id.shirts_button);
-        tshirt_bt = (ImageButton)findViewById(R.id.tshirt_button);
-        sweater_bt = (ImageButton)findViewById(R.id.sweater_button);
-        vest_bt = (ImageButton)findViewById(R.id.vest_button);
+        /* 2) 버튼 값 아이디로 가져와서 넣기 */
 
-        // 피부색 버튼
-        skin_btn_1 = (ImageButton)findViewById(R.id.skin_btn_1);
-        skin_btn_2 = (ImageButton)findViewById(R.id.skin_btn_2);
-        skin_btn_3 = (ImageButton)findViewById(R.id.skin_btn_3);
-        skin_btn_4 = (ImageButton)findViewById(R.id.skin_btn_4);
-        skin_btn_5 = (ImageButton)findViewById(R.id.skin_btn_5);
-
-        // 눈 버튼
-        eye_btn_1 = (ImageButton) findViewById(R.id.eye_btn_1);
-        eye_btn_2 = (ImageButton) findViewById(R.id.eye_btn_2);
-        eye_btn_3 = (ImageButton) findViewById(R.id.eye_btn_3);
-        eye_btn_4  = (ImageButton) findViewById(R.id.eye_btn_4);
-
-        // 입 버튼
-        mouth_btn_1 = (ImageButton) findViewById(R.id.mouth_btn_1);
-        mouth_btn_2 = (ImageButton) findViewById(R.id.mouth_btn_2);
-        mouth_btn_3 = (ImageButton) findViewById(R.id.mouth_btn_3);
-        mouth_btn_4 = (ImageButton) findViewById(R.id.mouth_btn_4);
-
-        //버튼에 클릭 이벤트 처리
-        shirts_bt.setOnClickListener(this);
-        tshirt_bt.setOnClickListener(this);
-        sweater_bt.setOnClickListener(this);
-        vest_bt.setOnClickListener(this);
-
-        //xml에 불러운 이미지뷰를 iv에 넣어 속성 사용하기 위한 구문
-        iv = (ImageView)findViewById(R.id.top_view);
-        skin_p = (ImageView)findViewById(R.id.skin); // 피부색
-        hair_view = (ImageView) findViewById(R.id.hair_view); // 머리
-        eye_view = (ImageView) findViewById(R.id.eye_view); // 눈
-        mouth_view = (ImageView) findViewById(R.id.mouth_view); // 입
-
-        // 레이아웃
+        // 0-1. 레이아웃
         linearLayout1 = (LinearLayout)findViewById(R.id.linearLayout1);
         linearLayout2 = (LinearLayout)findViewById(R.id.linearLayout2);
         linearLayout3 = (LinearLayout)findViewById(R.id.linearLayout3);
         linearLayout4 = (LinearLayout)findViewById(R.id.linearLayout4);
         linearLayout5 = (LinearLayout)findViewById(R.id.linearLayout5);
+        linearLayout6 = (LinearLayout)findViewById(R.id.linearLayout6);
+        linearLayout7 = (LinearLayout)findViewById(R.id.linearLayout7);
 
-        // 상단 버튼
+        // 0-2. 상단 버튼
         skin_button = (Button)findViewById(R.id.skin_button);
         hair_button = (Button) findViewById(R.id.hair_button);
         eye_button = (Button) findViewById(R.id.eye_button);
         mouth_button = (Button)findViewById(R.id.mouth_button);
         top_button = (Button)findViewById(R.id.top_button);
         bottom_button = (Button) findViewById(R.id.bottom_button);
+        bg_button = (Button)findViewById(R.id.bg_button);
 
-        // 머리 버튼
+        // 1. 피부색 버튼
+        skin_btn_1 = (ImageButton)findViewById(R.id.skin_btn_1);
+        skin_btn_2 = (ImageButton)findViewById(R.id.skin_btn_2);
+        skin_btn_3 = (ImageButton)findViewById(R.id.skin_btn_3);
+        skin_btn_4 = (ImageButton)findViewById(R.id.skin_btn_4);
+        skin_btn_5 = (ImageButton)findViewById(R.id.skin_btn_5);
+
+        // 2. 머리 버튼
         hair_btn_1 = (ImageButton)findViewById(R.id.hair_btn_1);
         hair_btn_2 = (ImageButton)findViewById(R.id.hair_btn_2);
         hair_btn_3 = (ImageButton) findViewById(R.id.hair_btn_3);
         hair_btn_4 = (ImageButton) findViewById(R.id.hair_btn_4);
         hair_btn_5 = (ImageButton) findViewById(R.id.hair_btn_5);
 
+        // 3. 눈 버튼
+        eye_btn_1 = (ImageButton) findViewById(R.id.eye_btn_1);
+        eye_btn_2 = (ImageButton) findViewById(R.id.eye_btn_2);
+        eye_btn_3 = (ImageButton) findViewById(R.id.eye_btn_3);
+        eye_btn_4  = (ImageButton) findViewById(R.id.eye_btn_4);
 
-        // 피부색 버튼 :: 이벤트 리스너
+        // 4. 입 버튼
+        mouth_btn_1 = (ImageButton) findViewById(R.id.mouth_btn_1);
+        mouth_btn_2 = (ImageButton) findViewById(R.id.mouth_btn_2);
+        mouth_btn_3 = (ImageButton) findViewById(R.id.mouth_btn_3);
+        mouth_btn_4 = (ImageButton) findViewById(R.id.mouth_btn_4);
+
+        // 5. 상의 버튼
+        shirts_bt = (ImageButton)findViewById(R.id.shirts_button);
+        tshirt_bt = (ImageButton)findViewById(R.id.tshirt_button);
+        sweater_bt = (ImageButton)findViewById(R.id.sweater_button);
+        vest_bt = (ImageButton)findViewById(R.id.vest_button);
+
+        // 6. 하의 버튼
+        bottom_btn_1 = (ImageButton) findViewById(R.id.short_pants);
+        bottom_btn_2 = (ImageButton) findViewById(R.id.slacks);
+        bottom_btn_3 = (ImageButton) findViewById(R.id.jean);
+        bottom_btn_4 = (ImageButton) findViewById(R.id.check_skirt);
+        bottom_btn_5 = (ImageButton) findViewById(R.id.tennis_skirt);
+
+        // 7. 배경색 버튼
+        bg_btn_1 = (ImageButton) findViewById(R.id.bg_btn_1);
+        bg_btn_2 = (ImageButton) findViewById(R.id.bg_btn_2);
+        bg_btn_3 = (ImageButton) findViewById(R.id.bg_btn_3);
+        bg_btn_4 = (ImageButton) findViewById(R.id.bg_btn_4);
+        bg_btn_5 = (ImageButton) findViewById(R.id.bg_btn_5);
+        bg_btn_6 = (ImageButton) findViewById(R.id.bg_btn_6);
+        bg_btn_7 = (ImageButton) findViewById(R.id.bg_btn_7);
+        bg_btn_8 = (ImageButton) findViewById(R.id.bg_btn_8);
+
+        //버튼에 클릭 이벤트 처리
+        shirts_bt.setOnClickListener(this);
+        tshirt_bt.setOnClickListener(this);
+        sweater_bt.setOnClickListener(this);
+        vest_bt.setOnClickListener(this);
+        bottom_btn_1.setOnClickListener(this);
+        bottom_btn_2.setOnClickListener(this);
+        bottom_btn_3.setOnClickListener(this);
+        bottom_btn_4.setOnClickListener(this);
+        bottom_btn_5.setOnClickListener(this);
+        bg_btn_1.setOnClickListener(this);
+        bg_btn_2.setOnClickListener(this);
+        bg_btn_3.setOnClickListener(this);
+        bg_btn_4.setOnClickListener(this);
+        bg_btn_5.setOnClickListener(this);
+        bg_btn_6.setOnClickListener(this);
+        bg_btn_7.setOnClickListener(this);
+        bg_btn_8.setOnClickListener(this);
+
+        //xml에 불러운 이미지뷰를 iv에 넣어 속성 사용하기 위한 구문
+        skin_p = (ImageView)findViewById(R.id.skin); // 피부색
+        hair_view = (ImageView) findViewById(R.id.hair_view); // 머리
+        eye_view = (ImageView) findViewById(R.id.eye_view); // 눈
+        mouth_view = (ImageView) findViewById(R.id.mouth_view); // 입
+        top_view = (ImageView)findViewById(R.id.top_view); //상의
+        bottom_view = (ImageView)findViewById(R.id.bottom_view); //하의
+        bg_view = (ImageView)findViewById(R.id.bg_view); //배경색
+
+
+       // 피부색 버튼 :: 이벤트 리스너
         skin_btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -232,6 +273,8 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
                 linearLayout3.setVisibility(View.GONE);
                 linearLayout4.setVisibility(View.GONE);
                 linearLayout5.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
 
                 linearLayout1.setVisibility(View.VISIBLE);
             }
@@ -244,6 +287,8 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
                 linearLayout3.setVisibility(View.GONE);
                 linearLayout4.setVisibility(View.GONE);
                 linearLayout5.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
 
                 linearLayout2.setVisibility(View.VISIBLE);
             }
@@ -256,6 +301,8 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
                 linearLayout2.setVisibility(View.GONE);
                 linearLayout4.setVisibility(View.GONE);
                 linearLayout5.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
 
                 linearLayout3.setVisibility(View.VISIBLE);
             }
@@ -268,6 +315,8 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
                 linearLayout2.setVisibility(View.GONE);
                 linearLayout3.setVisibility(View.GONE);
                 linearLayout5.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
 
                 linearLayout4.setVisibility(View.VISIBLE);
             }
@@ -280,8 +329,38 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
                 linearLayout2.setVisibility(View.GONE);
                 linearLayout3.setVisibility(View.GONE);
                 linearLayout4.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
 
                 linearLayout5.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bottom_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout1.setVisibility(View.GONE);
+                linearLayout2.setVisibility(View.GONE);
+                linearLayout3.setVisibility(View.GONE);
+                linearLayout4.setVisibility(View.GONE);
+                linearLayout5.setVisibility(View.GONE);
+                linearLayout7.setVisibility(View.GONE);
+
+                linearLayout6.setVisibility(View.VISIBLE);
+            }
+        });
+
+        bg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout1.setVisibility(View.GONE);
+                linearLayout2.setVisibility(View.GONE);
+                linearLayout3.setVisibility(View.GONE);
+                linearLayout4.setVisibility(View.GONE);
+                linearLayout6.setVisibility(View.GONE);
+                linearLayout5.setVisibility(View.GONE);
+
+                linearLayout7.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -291,19 +370,74 @@ public class StartActivity extends AppCompatActivity implements OnClickListener 
         switch (v.getId()) {
             case R.id.shirts_button:
                 //이미지뷰에 사진을 출력하는 구문
-                iv.setImageResource(R.drawable.shirts);
+                top_view.setImageResource(R.drawable.shirts);
                 break;
             case R.id.tshirt_button:
                 //이미지뷰에 사진을 출력하는 구문
-                iv.setImageResource(R.drawable.tshirt);
+                top_view.setImageResource(R.drawable.tshirt);
                 break;
             case R.id.sweater_button:
                 //이미지뷰에 사진을 출력하는 구문
-                iv.setImageResource(R.drawable.sweater);
+                top_view.setImageResource(R.drawable.sweater);
                 break;
             case R.id.vest_button:
                 //이미지뷰에 사진을 출력하는 구문
-                iv.setImageResource(R.drawable.vest);
+                top_view.setImageResource(R.drawable.vest);
+                break;
+
+            case R.id.check_skirt:
+                //이미지뷰에 사진을 출력하는 구문
+                bottom_view.setImageResource(R.drawable.check_skirt);
+                break;
+            case R.id.short_pants:
+                //이미지뷰에 사진을 출력하는 구문
+                bottom_view.setImageResource(R.drawable.shortpants);
+                break;
+            case R.id.jean:
+                //이미지뷰에 사진을 출력하는 구문
+                bottom_view.setImageResource(R.drawable.jean);
+                break;
+            case R.id.slacks:
+                //이미지뷰에 사진을 출력하는 구문
+                bottom_view.setImageResource(R.drawable.slacks);
+                break;
+            case R.id.tennis_skirt:
+                //이미지뷰에 사진을 출력하는 구문
+                bottom_view.setImageResource(R.drawable.tennis_skirt);
+                break;
+
+            case R.id.bg_btn_1:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_1);
+                break;
+            case R.id.bg_btn_2:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_2);
+                break;
+            case R.id.bg_btn_3:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_3);
+                break;
+            case R.id.bg_btn_4:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_4);
+                break;
+            case R.id.bg_btn_5:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_5);
+                break;
+
+            case R.id.bg_btn_6:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_6);
+                break;
+            case R.id.bg_btn_7:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_7);
+                break;
+            case R.id.bg_btn_8:
+                //이미지뷰에 사진을 출력하는 구문
+                bg_view.setImageResource(R.drawable.bg_8);
                 break;
         }
     }
